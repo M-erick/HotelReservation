2490a0ec8422b2f40c49ffbd46f76c296bfd06ce
EmillyN22 <muruginjue627@gmail.com>
2023-01-18 22:39:38 +0300

    done

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -36,59 +36,13 @@
                 </div>
             </div>
         </div>
-        <div class="search-open">
-            <div class="overlay"></div>
-            <div class="search-open-inner">
-                <div class="full-header">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-2 col-sm-6 col-xs-10">
-                                <div class="logo">
-                                    <a href="index-2.html"><img src="{{ asset('user/images/logo-2.png') }}" alt="Tinos"></a>
-                                    <div  id="booking2" class="booking reserve-booking">
-                                        <a class="btn" href="#">BO<span>OK</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-10 col-sm-6 col-xs-2">
-                                <div class="full-navigation-inner clearfix">
-                                    <div id="clapes-close" class="clapess2 clearfix">
-                                        <a href="#"><i class="fa fa-times"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="open-search-inner">
-                    <div class="container">
-                        <div class="col-md-12">
-                            <div  class="enter-search">
-                                <p>What are you looking for?</p>
-                                <input type="text" placeholder="Enter Your Search Here"/>
-                                <input class="btn" type="submit" value="Search Now" />
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="full-navgtion">
             <div class="overlay"></div>
             <div class="search-open-inner">
                 <div class="full-header">
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="col-md-2 col-sm-6 col-xs-10">
-                                <div class="logo">
-                                    <a href="{{ Route ('home') }}"><img src="{{ asset('user/images/logo1.png') }}" alt="Tinos"></a>
-                                    <div  id="booking3" class="booking reserve-booking">
-                                        <a class="btn" href="#">BO<span>OK</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-10 col-sm-6 col-xs-2">
+                            <div class="col-md-12 col-sm-6 col-xs-12">
                                 <div class="full-navigation-inner clearfix">
                                     <div id="clapess2" class="clapess2 clearfix">
                                         <a href="#"><i class="fa fa-times"></i></a>
@@ -104,26 +58,26 @@
                             <div class="col-md-6 col-sm-6 col-xs-6">
                                 <div class="full-navgtion-left">
                                     <ul>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="{{ Route ('home') }}">Home</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="about-us.html">About Us</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="{{ Route ('services') }}">All Serivces</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="wellness-and-spa.html">Services Details</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="our-packages.html">Packages</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="package-details.html">Packages Detail</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="photo-section.html">Gallery</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="blog.html">News</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="blog-detail.html">News Details</a></li>
+                                        <li> <a href="{{ Route ('home') }}">Home</a></li>
+                                        <li> <a href="about-us.html">About Us</a></li>
+                                        <li> <a href="{{ Route ('services') }}">All Serivces</a></li>
+                                        <li> <a href="wellness-and-spa.html">Services Details</a></li>
+                                        <li> <a href="our-packages.html">Packages</a></li>
+                                        <li> <a href="package-details.html">Packages Detail</a></li>
+                                        <li> <a href="photo-section.html">Gallery</a></li>
+                                        <li> <a href="blog.html">News</a></li>
+                                        <li> <a href="blog-detail.html">News Details</a></li>
                                     </ul>
                                 </div>
                             </div>
                             <div class="col-md-6 col-sm-6 col-xs-6">
                                 <div class="full-navgtion-left">
                                     <ul>
-                                        <li><i class="fa fa-play-circle-o"></i> <a id="btn-closs" href="#">Search Open Page</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="wellness-and-spa.html">Wellness & Spa</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="booking-room-selector.html">Booking Page</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="booking-details.html">Booking Detail</a></li>
-                                        <li><i class="fa fa-play-circle-o"></i> <a href="contact-us.html">Contact And Directions</a></li>
+                                        <li> <a id="btn-closs" href="#">Search Open Page</a></li>
+                                        <li> <a href="wellness-and-spa.html">Wellness & Spa</a></li>
+                                        <li> <a href="booking-room-selector.html">Booking Page</a></li>
+                                        <li> <a href="booking-details.html">Booking Detail</a></li>
+                                        <li> <a href="contact-us.html">Contact And Directions</a></li>
                                     </ul>
                                 </div>
                                 <div class="socail-infor">
@@ -171,9 +125,9 @@
                                     <div class="hedding">
                                         <div class="inner-hadding">
                                             <h2>Our Services</h2>
-                                            <span>Best Place TO Enjoy Your Life</span>
+                                            <span>Everything. Right, where you need it.</span>
                                         </div>
-                                        <p>Phasellus enim libero, blandit vel sapienvitae, condimen tum ultricies magna et.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <p>Experience the passion of hospitality.</p>
                                     </div>
                                 </div>
                             </div>
@@ -185,7 +139,6 @@
                                         </div>
                                         <div class="text-box">
                                             <h3><a href="wellness-and-spa.html">Hotel Parking</a></h3>
-                                            <p>De-Stress Retreat</p>
                                         </div>
                                     </div>
                                 </div>
@@ -196,7 +149,6 @@
                                         </div>
                                         <div class="text-box">
                                             <h3><a href="wellness-and-spa.html">Wellness & Spa</a></h3>
-                                            <p>De-Stress Retreat</p>
                                         </div>
                                     </div>
                                 </div>
@@ -207,7 +159,6 @@
                                         </div>
                                         <div class="text-box">
                                             <h3><a href="wellness-and-spa.html">Private Gym</a></h3>
-                                            <p>De-Stress Retreat</p>
                                         </div>
                                     </div>
                                 </div>
@@ -218,7 +169,6 @@
                                         </div>
                                         <div class="text-box">
                                             <h3><a href="wellness-and-spa.html">Free internet Wifi</a></h3>
-                                            <p>De-Stress Retreat</p>
                                         </div>
                                     </div>
                                 </div>
@@ -229,7 +179,6 @@
                                         </div>
                                         <div class="text-box">
                                             <h3><a href="wellness-and-spa.html">Dinning Retrate</a></h3>
-                                            <p>De-Stress Retreat</p>
                                         </div>
                                     </div>
                                 </div>
@@ -240,20 +189,13 @@
                                         </div>
                                         <div class="text-box">
                                             <h3><a href="wellness-and-spa.html">Online reservation</a></h3>
-                                            <p>De-Stress Retreat</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="load-more">
-                                        <a class="btn" href="#">Load More</a>
-                                    </div>
-                                </div>
-                            </div>
                         </section>
-                        <section class="newslatter-section">
+
+                        <!--<section class="newslatter-section">
                             <div class="row">
                                 <div class="col-md-10 col-md-offset-1">
                                     <div class="inner-news">
@@ -273,17 +215,17 @@
                                     </div>
                                 </div>
                             </div>
-                        </section>
+                        </section>-->
                     </div>
                 </main>
             </div>
-            <footer id="xp-footer" class=""  data-parallax="scroll" data-image-src="{{ asset('user/images/footer.jpg') }}">
+            <footer id="xp-footer" class=""  data-parallax="scroll" >
                 <div class="overlay"></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-md-5 col-sm-5 col-xs-12">
                             <div class="footer-inner">
-                                <img src="{{ asset('user/images/footer-logo.png') }}" alt="" />
+                                <img src="{{ asset('user/images/footer-logo-1.png') }}" alt="" />
                                 <p><span>E & M - </span>Copyright 2023. Developed by <a href="#">Emilly</a></p>
                             </div>
                         </div>
@@ -291,7 +233,6 @@
                             <div class="footer-inner">
                                 <ul>
                                     <li><a href="{{ Route ('home') }}">Home </a></li>
-                                    <li><a href="#">Hotel</a></li>
                                     <li><a href="{{ Route ('services') }}">Services</a></li>
                                     <li><a href="our-packages.html">Packages</a></li>
                                     <li><a href="about-us.html">About </a></li>
@@ -339,7 +280,7 @@
                                                     <h2>Reserve Now</h2>
                                                     <span>Best Place TO Enjoy Your Life</span>
                                                 </div>
-                                                <p>Phasellus enim libero, blandit vel sapienvitae, condimen tum ultricies magna et.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                <p>Twice the comfort, twice the value, twice the hotel. <br> Let us stay with you.</p>
                                             </div>
                                         </div>
                                     </div>
