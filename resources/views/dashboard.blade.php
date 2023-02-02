e7a9a0aa1d1c7eef6dbffc6db14633f7737f7834
eric <61746940+M-erick@users.noreply.github.com>
2023-02-02 14:51:38 +0300

    a segment of home page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,73 @@
 <x-app-layout>
 
-    
+    <section id="banner" class="banner-2">
+        <div class="bainner-home">
+            <div id="owl-demo" class="owl-carousel owl-theme">
+                <div class="item" style="background: url('{{ asset('images/home-bg.jpg') }}') center no-repeat; background-size: cover; ">
+                    <div class="ovrlay"></div>
+                    <div class="banner-text">
+                        <div class="chid-pernt">
+                            <div class="child">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="text-box">
+                                                <h2> HELLO </h2>
+                                                <h1>WELCOME TO E & M HOTEL</h1>
+                                                <p>Best Place TO Enjoy Your Life</p>
+                                                <strong>Welcome</strong>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item" style="background: url('{{ asset('images/homae-bg.jpg') }}') center no-repeat; background-size: cover; ">
+                    <div class="ovrlay"></div>
+                    <div class="banner-text">
+                        <div class="chid-pernt">
+                            <div class="child">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="text-box">
+                                                <h2>Come & Enjoy</h2>
+                                                <h1>Create Unforgettable Memories With Us</h1>
+                                                <p>Take Time To Relax</p>
+                                                <strong>Relax</strong>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item" style="background: url('{{ asset('images/home.jpg') }}') center no-repeat; background-size: cover; ">
+                    <div class="ovrlay"></div>
+                    <div class="banner-text">
+                        <div class="chid-pernt">
+                            <div class="child">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="text-box">
+                                                <h2>Luxury. Redefined</h2>
+                                                <h1>Stay With Us, Feel At Home</h1>
+                                                <p>Enjoy Your Stay</p>
+                                                <strong>Enjoy</strong>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </x-app-layout>
