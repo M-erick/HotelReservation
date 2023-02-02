4c26fe5415b65bd0078088e7e9dc4ea211810686
eric <61746940+M-erick@users.noreply.github.com>
2023-02-02 15:49:39 +0300

    Footer update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,12 @@
 
 
             {{-- <p><span> </span>2023. Developed by <a href="#">Emilly</a></p> --}}
-          <p><i>E & M-</i> &copy; 2022 - {{ date('Y') }}.All Rights Reserved</p>
+          {{-- <p><i>E & M-</i> &copy; 2022 - {{ date('Y') }}.All Rights Reserved</p> --}}
+          <div class="shrink-0 flex items-left h-9">
+            <img src="{{ asset('images/footer-logo-1.png') }}" alt="" />
+
+            <p><span>E & M - </span>&copy; 2022 - {{ date('Y') }}.All Rights Reserved <a href="{{ Route ('home') }}"></a></p>
+        </div>
 
 
 
