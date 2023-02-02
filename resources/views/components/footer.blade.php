6d9a3678d501df7461c76c8696793110324be21b
eric <61746940+M-erick@users.noreply.github.com>
2023-02-02 15:13:58 +0300

    Footer Component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+
+    <div class="text-center">
+
+
+            {{-- <p><span> </span>2023. Developed by <a href="#">Emilly</a></p> --}}
+          <p><i>E & M-</i> &copy; 2022 - {{ date('Y') }}.All Rights Reserved</p>
+
+
+
+    </div>
+</div>
