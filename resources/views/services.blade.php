7abe61f9f0a35fa7d574e85774b3e54db47bbb79
eric <61746940+M-erick@users.noreply.github.com>
2023-02-06 13:21:22 +0300

    modified Changes

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -26,16 +26,7 @@
     </head>
     <body>
 
-        <div class="spinner-wrapper">
-            <div class="spinner">
-                <div class="sk-folding-cube">
-                    <div class="sk-cube1 sk-cube"></div>
-                    <div class="sk-cube2 sk-cube"></div>
-                    <div class="sk-cube4 sk-cube"></div>
-                    <div class="sk-cube3 sk-cube"></div>
-                </div>
-            </div>
-        </div>
+        
 
         <div class="full-navgtion">
             <div class="overlay"></div>
